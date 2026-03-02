2f4b8f5634212098066ed15209ba4ce627199e7b
spano14 <67838888+spano14@users.noreply.github.com>
2026-03-02 22:29:00 +0100

    fix

--- resources/views/filament/pages/qr-codes.blade.php
+++ resources/views/filament/pages/qr-codes.blade.php
@@ -53,20 +53,11 @@
 
                                 {{-- Actions --}}
                                 <div class="flex flex-wrap items-center gap-1.5 pt-0.5">
-                                    <a href="{{ $item['url'] }}" target="_blank"
-                                       class="inline-flex items-center gap-1 rounded border border-gray-200 bg-white px-1.5 py-0.5 text-[11px] font-medium text-gray-600 hover:bg-gray-100 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:hover:bg-gray-800"
-                                       title="Open page">
-                                        <svg style="width: 14px; height: 14px;" xmlns="http://www.w3.org/2000/svg" class="inline" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.8">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"/>
-                                        </svg>
-                                        Visit
-                                    </a>
+                                    
                                     <a href="{{ $item['downloadUrl'] }}"
                                        class="inline-flex items-center gap-1 rounded bg-primary-50 px-1.5 py-0.5 text-[11px] font-semibold text-primary-700 hover:bg-primary-100 dark:bg-primary-900/30 dark:text-primary-400 dark:hover:bg-primary-900/50"
                                        title="Download PNG">
-                                        <svg style="width: 14px; height: 14px;" xmlns="http://www.w3.org/2000/svg" class="inline" viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"/>
-                                        </svg>
+                                       
                                         PNG
                                     </a>
                                 </div>
