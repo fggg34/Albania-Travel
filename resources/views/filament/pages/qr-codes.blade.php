186f86e36a6b9aa2d2d9f64f2034e161235b62ac
spano14 <67838888+spano14@users.noreply.github.com>
2026-03-02 22:19:47 +0100

    fix qr code 333

--- resources/views/filament/pages/qr-codes.blade.php
+++ resources/views/filament/pages/qr-codes.blade.php
@@ -63,7 +63,7 @@
                                 <a href="{{ $item['downloadUrl'] }}"
                                    class="rounded bg-primary-50 px-2 py-1 text-[11px] font-semibold text-primary-700 hover:bg-primary-100 dark:bg-primary-900/30 dark:text-primary-400 dark:hover:bg-primary-900/50"
                                    title="Download PNG">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="inline h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
+                                    <svg style="width: 20px; height: 20px;" xmlns="http://www.w3.org/2000/svg" class="inline h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
                                         <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"/>
                                     </svg>
                                     PNG
