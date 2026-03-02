329a8ece05f58d3da3e9cbd8e159e5faba9ab0ac
spano14 <67838888+spano14@users.noreply.github.com>
2026-03-02 20:48:45 +0100

    fix single city

--- resources/views/pages/cities/show.blade.php
+++ resources/views/pages/cities/show.blade.php
@@ -234,8 +234,8 @@
             @endif
         </div>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            @foreach($city->tours->take(6) as $tour)
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+            @foreach($city->tours->take(8) as $tour)
             <x-tour-card :tour="$tour" :queryParams="[]" :wishlisted="false" :slider="false" />
             @endforeach
         </div>
